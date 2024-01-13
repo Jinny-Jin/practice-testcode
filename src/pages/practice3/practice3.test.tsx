6d2117ca18109bf79cf7e6ed7544266e010b90a1
Jinny-Jin <katie9796@naver.com>
2024-01-14 06:57:25 +0900

    예제3 - 두번째 테스트 완료

--- src/pages/practice3/practice3.test.tsx
+++ src/pages/practice3/practice3.test.tsx
@@ -1,6 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Test3 from './practice3';
 import "@testing-library/jest-dom/extend-expect"
+import { check } from 'ts-toolbelt/out/Test';
 
 describe("Test3", ()=> {
     test("Label과 Input 그리고 버튼이 제대로 렌더링 되어야 함", ()=> {
@@ -18,12 +19,24 @@ describe("Test3", ()=> {
         expect(button).toBeInTheDocument()
 
     })
-    test.skip("이름을 입력하고 약관에 동의한 다음 버튼을 클릭하면 alert 창으로 입력한 값이 출력되어야 함",()=>{
+    test("이름을 입력하고 약관에 동의한 다음 버튼을 클릭하면 alert 창으로 입력한 값이 출력되어야 함",()=>{
         //arrange
+        const alertMock = jest.fn()
+        window.alert = alertMock
+
+        render(<Test3/>)
+        const nameInput = screen.getByRole("textbox")
+        const checkbox = screen.getByRole("checkbox")
+        const button = screen.getByRole("button")
 
         //act
+        fireEvent.change(nameInput, {target : {value : "Jin"}})
+        fireEvent.click(checkbox)
+        fireEvent.click(button)
 
         //assert
+        expect(alertMock).toHaveBeenCalledWith("name: Jin")
+
     })
     test.skip("약관에 동의하지 않으면 alert 창이 출력되지 말아야 함",()=>{
         //arrange
